8aa57508e7217132e32a1783f2a3839639b0bf56
Jorge Jardines <jorge.jardines@frontiersin.org>
2016-03-01 15:32:12 +0100

    add StackExchange empty class

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 C# clients
 ----------
 source: http://redis.io/clients#c
-* ServiceStack.Redis
+* ServiceStack.Redis V3 ( https://github.com/ServiceStackV3/ServiceStackV3 )
 
 
 Gui client
@@ -12,5 +12,6 @@ Some useful articles
 ---------------------
 * http://redis.io/topics/lru-cache
 * http://redis.io/topics/data-types-intro
+* https://servicestack.net/pricing
 
 
---
+++ RedisClient/StackExchangeRedisCacheManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RedisClient
+{
+    public class StackExchangeRedisCacheManager : ICacheManager, IRedisCacheManager
+    {
+
+        public StackExchangeRedisCacheManager()
+        {
+        }
+
+        public bool Add<TItem>(string key, TItem item, TimeSpan expirationPeriod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TItem Get<TItem>(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
